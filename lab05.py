fa88f979f2a94a21677f58aa2b237c3dbb3fc00b
MarcosValenzuela <marcosvalezuela@mail.fresnostate.edu>
2020-12-01 17:43:10 -0800

    finished psuedo-code

--- lab05.py
+++ lab05.py
@@ -1,17 +1,22 @@
-""""
-The program is trying to translate a sentence captured an user input.
-We first read text file.
-then from the text fule, we create a list of strings from each lines in the text file.
-Then we create dictionary from list.
-Once the text file has been read into memory, we close the file.
+"""
 
-we then define a function for translating which envolves splitting the user input (sentence) into 
-an array of strings ["enjoy", "the", "excellent", "band", "tonight"]
+main():
+    set sentence = input()
+    set dictionary = translate()
+    translate(setence, dictionary)
 
-once we have the array of strings representing the user's input setence, we
-loop through each words, find the key matching the word and rreturn back the value tied to the word
-in our dictionary
+translate(sentence, dictionary):
+    words = for each of the word in the sentence
+    for each words, translate the word
+    print translated sentence to user
 
-after each word is trnaslated, we then
-print out the translated sentence to the user.
+create_dictionary():
+    read in textese.txt
+    create list = each line from file
+    close the file
+    create a dict off of the list
+    return the dict
+
+main()
+
 """
